52292870becd4f672925f84279098cc57af05ca6
Aryan Gupta <105268630+wolf9652@users.noreply.github.com>
2024-02-22 14:39:10 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {fontFamily: {
+      mullish: ["Mullish", "sans-serif"],
+    },
+    colors:{
+      deepblue: "#02042a",
+      lightblue: "#2b84ea",
+      lightBlue300:"#4b94ed",
+      lightBlue500: "#0b72e7",
+      greenLight: "#61cea6" ,
+      grayText: "#818597",
+      lightGray: "#e2e2e2",
+      grayBlue: "#344a6c" ,
+      deepBlueHead: "#162f56",
+      gray2: "#525a76",
+      blue: "#5db7f9",
+      lblue: "#2998fe",
+      dblue: "#181c2e",
+    },
+  },
+  },
+  plugins: [],
+}
+
